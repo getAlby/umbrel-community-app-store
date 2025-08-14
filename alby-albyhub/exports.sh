06ad19654cab510cbcfaef0f6c72ea91888cdd65
Nodii <13803869+NodeDiver@users.noreply.github.com>
2025-08-14 00:40:31 -0300

    changed to LN and added cautious syntax in compose

--- alby-albyhub/exports.sh
+++ alby-albyhub/exports.sh
@@ -5,5 +5,5 @@ installed_apps=$("${UMBREL_ROOT}/scripts/app" ls-installed)
 
 if echo "$installed_apps" | grep --quiet 'lightning'; then
   export APP_ALBYHUB_LN_BACKEND="LND"
-  export APP_ALBYHUB_LND_ADDRESS="10.21.21.9:10009"
+  export APP_ALBYHUB_LN_ADDRESS="10.21.21.9:10009"
 fi
