50de5f2de39d59c06a647f028d44288ba79ed621
Nodii <13803869+NodeDiver@users.noreply.github.com>
2025-08-14 00:36:00 -0300

    trying a minimalistic approach, based on bumi recomendations

--- alby-albyhub/exports.sh
+++ alby-albyhub/exports.sh
@@ -1,18 +1,9 @@
-#!/bin/bash
+export APP_ALBYHUB_LN_BACKEND="LDK"
 
-# Check if Lightning Node app is installed and export required variables if so
-# The Lightning Node app is optional and not listed in the `required` field of the umbrel-app.yml file, so we need to do this for compatibility with umbrelOS >=1.3 where only exports of required apps are sourced.
+# Checks for lightning(LND) installation in Umbrel, just like the mempool app does.
 installed_apps=$("${UMBREL_ROOT}/scripts/app" ls-installed)
 
 if echo "$installed_apps" | grep --quiet 'lightning'; then
-  # LND is available, export LND-specific variables
-  export APP_ALBY_LIGHTNING_AVAILABLE="true"
-  export APP_ALBY_LN_BACKEND_TYPE="LND"
-  export APP_ALBY_LIGHTNING_NODE_IP="10.21.21.9"
-  export APP_ALBY_LIGHTNING_NODE_GRPC_PORT="10009"
-  export APP_ALBY_LIGHTNING_NODE_REST_PORT="8080"
-else
-  # LND is not available, will use LDK backend
-  export APP_ALBY_LIGHTNING_AVAILABLE="false"
-  export APP_ALBY_LN_BACKEND_TYPE="LDK"
+  export APP_ALBYHUB_LN_BACKEND="LND"
+  export APP_ALBYHUB_LND_ADDRESS="10.21.21.9:10009"
 fi
