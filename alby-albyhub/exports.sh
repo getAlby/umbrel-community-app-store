59f3f2e419b0bffa973a5a219c16c854b8290233
Nodii <13803869+NodeDiver@users.noreply.github.com>
2025-08-15 01:53:26 -0300

    cleaned up exports.sh, it seems it was not sending correctly the variables to the compose

--- alby-albyhub/exports.sh
+++ alby-albyhub/exports.sh
@@ -4,19 +4,13 @@
 export APP_ALBYHUB_LN_BACKEND="LDK"
 export APP_ALBYHUB_LND_ADDRESS=""
 
-# Checks for lightning(LND) installation in Umbrel, just like the mempool app does.
-echo "Checking for lightning installation..."
+# Check if Lightning Node app is installed and export required variables if so
+
 installed_apps=$("${UMBREL_ROOT}/scripts/app" ls-installed)
-echo "Installed apps: '$installed_apps'"
 
 if echo "$installed_apps" | grep --quiet 'lightning'; then
-  echo "Lightning detected! Setting LND backend"
   export APP_ALBYHUB_LN_BACKEND="LND"
   export APP_ALBYHUB_LND_ADDRESS="10.21.21.9:10009"
-else
-  echo "No lightning detected, using LDK backend"
-  export APP_ALBYHUB_LN_BACKEND="LDK"
-  export APP_ALBYHUB_LND_ADDRESS=""
 fi
 
 echo "Final configuration:"
